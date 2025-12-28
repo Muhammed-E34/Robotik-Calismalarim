36b55eb4170dbdc423929b3b7d66e61b7b73a5ea
Muhammed-E34 <muhammederyilmaz3333@gmail.com>
2025-12-28 15:25:22 +0300

    Update basic_algorithms.py

--- python_egıtımı/basic_algorithms.py
+++ python_egıtımı/basic_algorithms.py
@@ -4,7 +4,5 @@ kullanici_sayisi = int(input("Faktöriyelini istediğin sayı: "))
 for sayi in range(1, kullanici_sayisi + 1):
     
     çarpim = çarpim * sayi
-    # Burası döngü içi (Sessizce topla)
 
-# Burası döngü dışı (Bittiği zaman konuş)
 print(f"faktöriyeli: {çarpim}")
